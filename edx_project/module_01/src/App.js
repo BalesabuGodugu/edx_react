fad5edefb9866fef04701d5680257a53035d4f6f
Balesabu <jssoftware@jssoftwares-MacBook-Air.local>
2017-09-28 01:20:50 +0530

    updated files

---
+++ edx_project/module_01/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={count:0}
+  }
+
+// call click event and update count 
+incrementCount(){
+  this.setState({
+    count:this.state.count+1
+  }
+    
+  )
+}
+
+  render() {
+    return (
+      <div className="App">
+        <p>count:{this.state.count}</p>
+        <button type="button" onClick={this.incrementCount.bind(this)}>clickhere</button>
+      </div>
+    );
+  }
+}
+
+export default App;
